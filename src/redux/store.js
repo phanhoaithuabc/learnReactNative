731adf86ee3c2a688e4e26be3819616b69e69043
phan hoai thu <phanhoaithuabc@gmail.com>
2022-02-20 16:35:15 +0700

    todo app

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,6 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import userReducer from './reducers';
 
-const rootReducer = combineReducers({ userReducer });
+const rootReducer = combineReducers({ taskReducer });
 
 export const Store = createStore(rootReducer, applyMiddleware(thunk));
--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -4,25 +4,51 @@ import { RNCamera } from 'react-native-camera';
 import { useCamera } from 'react-native-camera-hooks';
 import CustomButton from '../utils/CustomButton';
 import RNFS from 'react-native-fs';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTasks } from '../redux/actions';
 
 export default function Camera() {
 
     const [{ cameraRef }, { takePicture }] = useCamera(null);
+    // todo app
+    const { tasks } = useSelector(state => state.taskReducer);
+    const dispatch = useDispatch();
 
     const captureHandle = async () => {
         try {
             const data = await takePicture();
             console.log(data.uri);
             const filePath = data.uri;
-            const newFilePath = RNFS.ExternalDirectoryPath + '/MyTest.jpg';
-            RNFS.moveFile(filePath, newFilePath)
-                .then(() => {
-                    console.log('IMAGE MOVED', filePath, '-- to --', newFilePath);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
+
+            // learning
+            // const newFilePath = RNFS.ExternalDirectoryPath + '/MyTest.jpg';
+            // RNFS.moveFile(filePath, newFilePath)
+            //     .then(() => {
+            //         console.log('IMAGE MOVED', filePath, '-- to --', newFilePath);
+            //     })
+            //     .catch(error => {
+            //         console.log(error);
+            //     })
+
+            // todo app
+            updateTask(route.params.id, filePath);
         } catch (error) { console.log(error); }
+    }
+
+    // todo app
+    const updateTask = (id, path) => {
+        const index = tasks.findIndex(task => task.ID === id);
+        if (index > -1) {
+            let newTasks = [...tasks];
+            newTasks[index].Image = path;
+            AsyncStorage.setItem('Tasks', JSON.stringify(newTasks))
+                .then(() => {
+                    dispatch(setTasks(newTasks));
+                    Alert.alert('Success!', 'Task image is saved.');
+                    navigation.goBack();
+                })
+                .catch(err => console.log(err))
+        }
     }
 
     return (
